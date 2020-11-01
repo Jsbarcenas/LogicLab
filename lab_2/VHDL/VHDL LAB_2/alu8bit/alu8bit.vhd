661a8c6af00f430e7bf0992dc943b2f9c9932dc7
SibrimB13 <sibrimbarcenas@gmail.com>
2020-11-01 07:38:37 -0500

    Bug fixes v0.01

--- lab_2/VHDL/VHDL LAB_2/alu8bit/alu8bit.vhd
+++ lab_2/VHDL/VHDL LAB_2/alu8bit/alu8bit.vhd
@@ -3,31 +3,30 @@ use ieee.std_logic_1164.all;
 
 entity alu8bit is port(
 
-	A,B	: in std_logic;
+	Z,Y	: in std_logic;
 	sel	: in std_logic_vector(1 downto 0);
 	R		: out std_logic);
 end entity;
 
 architecture rtl of Alu8bit is
--- Component List
+
 
 component sum8bit is port(
-	A,B,Cin	: in std_logic;
+	Z,Y,Cin	: in std_logic;
 	sum, Cout: out std_logic
 );
 end component;
 
 
 component AND_8bit is port(
-	A,B	:	in std_logic_vector(7 downto 0);
+	Z,Y	:	in std_logic_vector(7 downto 0);
 	R		:	OUT std_logic_vector(7 downto 0);
 
 	);
 end component;
 
-
 component OR_8bit is port(
-	A,B	:	in std_logic_vector(7 downto 0);
+	Z,Y	:	in std_logic_vector(7 downto 0);
 	R		:	OUT std_logic_vector(7 downto 0);
 
 	);
@@ -35,8 +34,8 @@ end component;
 
 
 component comparador is port( 
-   A : in std_logic_vector(7 downto 0); 
-   B : in std_logic_vector(7 downto 0);
+   Z : in std_logic_vector(7 downto 0); 
+   Y : in std_logic_vector(7 downto 0);
    menor : out std_logic;  
    igual : out std_logic;   
    mayor:  out std_logic  
@@ -46,16 +45,13 @@ end component;
 
 
 component mux_8_1bit is port(
-	A : in std_logic_vector(7 downto 0);
+	Z : in std_logic_vector(7 downto 0);
 	Sel : in std_logic_vector(2 downto 0);
-	B : out std_logic
+	Y : out std_logic
 	);
 		
 end component;
 
-
-
--- signals
 	signal Cin_signal,Cout_signal: std_logic;
 	signal vector_mux	: std_logic_vector(7 downto 0);
 	
@@ -67,7 +63,7 @@ vector_mux(1)  <= OR_8bit;
 vector_mux(2) <= sum8bit ;
 vector_mux(3)  <= comparador;
 Cin_signal <= '0';
-sumador: sum8bit port map(A,B,Cin_signal,vector_mux(2),Cout_signal);
+sumador: sum8bit port map(Z,Y,Cin_signal,vector_mux(2),Cout_signal);
 Mux: mux_8_1bit port map(vector_mux,sel,R);
 
 end architecture;
